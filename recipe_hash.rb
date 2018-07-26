6b21e87a18b7c4087f449adaa31ae00af3bebbc1
april-guerin <aprilguerin@yahoo.com>
2018-07-26 20:51:28 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -21,7 +21,7 @@ def ingredients_and_amounts(perfect_10_recipe)
   perfect_10_recipe = { :almond_flour => "2 cups", :gluten_free_whole_oats => "1 cup", :kosher_salt => "1/2 tea spoon", :baking_powder => "1/2 tea spoon", :baking_soda => "1/4 tea spoon", :xanthan_Gum => "1/2 tea spoon", :slivered_almonds => "1/4 cup", :mini_dark_chocolate_chips => "3/4 cup", :olive_oil => "1/2 cup", :agave => "1/4 cup"}
   
   perfect_10_recipe.each do |ingredients|
-    puts ingredients
+    puts "#{ingredients.first} #{ingredients.last}"
   end
 end
 
